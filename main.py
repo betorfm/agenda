41f1bdd877eecd06a7a466194a0df7a859236390
betorfm <betorfm88@gmail.com>
2025-01-10 23:29:48 -0300

    atualização

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ class AgendaApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Agenda - CRUD com Tkinter e PostgreSQL")
-        self.root.geometry("900x500") # Tamanho inicial da janela
+        self.root.geometry("660x450") # Tamanho inicial da janela
+        self.root.columnconfigure(0, weight=1, minsize=10)
+        self.root.resizable(False, False) # mantem a janela no tamanho fixo
 
         #Elementos da Interface
         self.setup_ui()
@@ -14,33 +16,40 @@ class AgendaApp:
     def setup_ui(self):
         # Atribui pesos às colunas para expansão proporcional
         self.root.grid_columnconfigure(0, weight=1)  # Coluna 0 com peso
-        self.root.grid_columnconfigure(1, weight=1)  # Coluna 1 com peso maior
+        self.root.grid_columnconfigure(1, weight=1)  # Coluna 1 com peso
         self.root.grid_columnconfigure(2, weight=1)  # Coluna 2 com peso
         self.root.grid_columnconfigure(3, weight=1)  # Coluna 3 com peso
 
         # Campos de entrada
-        tk.Label(self.root, text="Nome", font=("Arial", 12, "bold"), bg="#f0f0f0", fg="#333", borderwidth=2, relief="solid").grid(row=0, column=0, padx=10, pady=10)
-        self.nome_entry = tk.Entry(self.root, width=80)
-        self.nome_entry.grid(row=0, column=1,columnspan=3, padx=10, pady=10)
+        tk.Label(self.root, text="Nome", font=("Arial", 15, "bold"), bg="#f0f0f0", fg="#333", borderwidth=2, relief="solid").grid(row=0, column=0, padx=10, pady=10)
+        self.nome_entry = tk.Entry(self.root, width=50)
+        self.nome_entry.grid(row=0, column=1, columnspan=3, padx=10, pady=10)
 
         tk.Label(self.root, text="Telefone").grid(row=1, column=0, padx=1, pady=10)
-        self.telefone_entry = tk.Entry(self.root, width=80)
+        self.telefone_entry = tk.Entry(self.root, width=50)
         self.telefone_entry.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
 
         tk.Label(self.root, text="Email").grid(row=2, column=0, padx=1, pady=10)
-        self.email_entry = tk.Entry(self.root, width=80)
+        self.email_entry = tk.Entry(self.root, width=50)
         self.email_entry.grid(row=2, column=1, columnspan=3, padx=10, pady=10)
 
+        # Frame para os botões
+        button_frame = tk.Frame(self.root)
+        button_frame.grid(row=3, column=0, columnspan=4, pady=1)
+
         # Botões CRUD
-        tk.Button(self.root, text="Criar", command=self.criar_contato, font=("Arial", 12, "bold"),
-          bg="#4CAF50", fg="white", activebackground="#45a049", activeforeground="white").grid(row=3, column=0, pady=10)
-        tk.Button(self.root, text="Ler", command=self.ler_contatos).grid(row=3, column=1, pady=10)
-        tk.Button(self.root, text="Atualizar", command=self.atualizar_contato).grid(row=3, column=2, pady=10)
-        tk.Button(self.root, text="Deletar", command=self.deletar_contato).grid(row=3, column=3, pady=10)
+        tk.Button(button_frame, text="Criar", command=self.criar_contato, font=("Arial", 12, "bold"), bg="#4CAF50", fg="white", activebackground="red", activeforeground="black").pack(side=tk.LEFT, padx=5)
+        tk.Button(button_frame, text="Ler", command=self.ler_contatos).pack(side=tk.LEFT, padx=5)
+        tk.Button(button_frame, text="Atualizar", command=self.atualizar_contato).pack(side=tk.LEFT, padx=5)
+        tk.Button(button_frame, text="Deletar", command=self.deletar_contato).pack(side=tk.LEFT, padx=5)
 
         # Tabela de dados
         self.tabela = ttk.Treeview(self.root, columns=("ID", "Nome", "Telefone", "Email"), show="headings")
         self.tabela.grid(row=5, column=0, columnspan=4, padx=10, pady=20)
+        self.tabela.column("ID", width="30", anchor="center", stretch="false")
+        self.tabela.column("Nome", width="250", anchor="center", stretch="false")
+        self.tabela.column("Telefone", width="150", anchor="center", stretch="false")
+        self.tabela.column("Email", width="200", anchor="center", stretch="false")
         self.tabela.heading("ID", text="ID")
         self.tabela.heading("Nome", text="Nome")
         self.tabela.heading("Telefone", text="Telefone")
